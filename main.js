c4f6c616d20ae90bea94493fd25aa047254a4161
KorotkyiAnton <anton.korotkyi@nure.ua>
2022-06-27 07:50:36 +0300

    Update -add realisation of addGotData(), deleteContactFunction() methods- (main.js)

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /**
  * decoration tools
  */
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbz4AuOmbYvzZsrcBOe6FO7EY3pVEAFISk8IegMIBk_iUARLarnK28nK1QroSJsAObXDEw/exec'; // Ссылка на развернутое веб-приложение gas
+
 window.onload = () => {
     //main logo
     const mainLogo = document.getElementById("main");
@@ -40,59 +42,21 @@ window.onload = () => {
 
     /**
      * main functionality
-     * 1: Показать все записи addGotData(JSON data)
+     * 1: Показать все записи addGotData(JSON data)                     ?COMPLETED?
      * 2: Добавления addPostData()
      * 3: Редактирования editContactFunction(HTMLButtonElement object)
-     * 4: Удаления deleteContactFunction(HTMLButtonElement object)
+     * 4: Удаления deleteContactFunction(HTMLButtonElement object)      ?COMPLETED?
      * 5: Поиск searchContact(string field)
      * 6: Экспорт exportContacts(string pathToFile)
      * 7: Импорт importContacts()
      * 8: Напоминалка reminder()
      */
 
-    //тут надо дописать ссылку и нормально соеденить его с addGotData(data)
-    // function showAllContacts() {
-    //     fetch("")
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             //addGotData(data);
-    //         })
-    // }
-    //showAllContacts();
-
-    function addGotData(data) {
-        listOfContact.innerHTML = "";
-        data.forEach((row, index) => {
-            listOfContact.innerHTML += "<li class=\"list-group-item mb-1\">\n" +
-                "            <div class=\"d-flex flex-row align-items-center\">\n" +
-                "                <a class=\"nav-link d-flex flex-fill\" data-bs-toggle=\"collapse\" href=\"#id" + index + "\">\n" +
-                row.first +
-                "                </a>\n" +
-                "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"editContactFunction(this)\" data-phone='" + row.fifth + "'>🖊</span>Edit</button>\n" +
-                "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"deleteContactFunction(this)\" data-phone='" + row.fifth + "'>🗑</span>Delete</button>\n" +
-                "            </div>\n" +
-                "            <div class=\"collapse\" id=\"id" + index + "\">\n" +
-                "                <div class=\"card card-body\">\n" +
-                "                    <p>Company: " + row.second +
-                "                    </p><p>Group: " + row.third +
-                "                    </p><p>Birthday: " + row.fourth +
-                "                    </p><p>Phone: " + row.fifth +
-                "                    </p><p>Email: " + row.sixth +
-                "                    </p><p>Address: " + row.seventh +
-                "                    </p><p>Last Call: " + row.eighth +
-                "                    </p><p>Addition Info: " + row.ninth +
-                "                    </p><p>Description: " + row.tenth +
-                "                </div>\n" +
-                "            </div>\n" +
-                "        </li>"
-        })
-    }
-
     /**
      * debugging
      * Это потом сотрете
      */
-    let data = '[{"first":"Anton", "second":"university", "third":"friend", "fourth":"2002.01.02", "fifth":"380664236782", "sixth":"anton.korotkyi@nure.ua", "seventh":"Barabolkina st.", "eighth":"2022.06.01", "ninth":"sdfsdf", "tenth":"fsdfsdf"}, {"first":"Daniil", "second":"university", "third":"friend", "fourth":"01.03.2002", "fifth":"380996327334", "sixth":"daniil.hurenko@nure.ua", "seventh":"Dostoevskii st.", "eighth":"2022.06.28", "ninth":"sdfsdf", "tenth":"fsdfsdf"}, {"first":"Daniil", "second":"university", "third":"friend", "fourth":"01.03.2002", "fifth":"380996327334", "sixth":"daniil.hurenko@nure.ua", "seventh":"Dostoevskii st.", "eighth":"2022.06.28", "ninth":"sdfsdf", "tenth":"fsdfsdf"}, {"first":"Daniil", "second":"university", "third":"friend", "fourth":"01.03.2002", "fifth":"380996327334", "sixth":"daniil.hurenko@nure.ua", "seventh":"Dostoevskii st.", "eighth":"2022.06.28", "ninth":"sdfsdf", "tenth":"fsdfsdf"}, {"first":"Daniil", "second":"university", "third":"friend", "fourth":"01.03.2002", "fifth":"380996327334", "sixth":"daniil.hurenko@nure.ua", "seventh":"Dostoevskii st.", "eighth":"2022.06.28", "ninth":"sdfsdf", "tenth":"fsdfsdf"}, {"first":"Daniil", "second":"university", "third":"friend", "fourth":"01.03.2002", "fifth":"380996327334", "sixth":"daniil.hurenko@nure.ua", "seventh":"Dostoevskii st.", "eighth":"2022.06.28", "ninth":"sdfsdf", "tenth":"fsdfsdf"}]';
+    let data = '[{"first":"Anton", "second":"university", "third":"friend", "fourth":"2002.01.02", "fifth":"380664236782", "sixth":"anton.korotkyi@nure.ua", "seventh":"Barabolkina st.", "eighth":"2022.06.01", "ninth":"sdfsdf", "tenth":"fsdfsdf"}, {"first":"Daniil", "second":"university", "third":"friend", "fourth":"01.03.2002", "fifth":"380996327334", "sixth":"daniil.hurenko@nure.ua", "seventh":"Dostoevskii st.", "eighth":"2022.06.28", "ninth":"sdfsdf", "tenth":"fsdfsdf"}]';
     addGotData(JSON.parse(data));
     /**
      * end of debugging
@@ -101,6 +65,46 @@ window.onload = () => {
     function addPostData() {
         //Тут наверное можно получить объект формы и запревентить сабмит, потом с помощью фетча отправить
     }
+}
+
+function showAllContacts() { 
+    fetch(scriptUrl)
+        .then(res => res.json())
+        .then(data => {
+            addGotData(data);
+        })
+}
+showAllContacts();
+
+function addGotData(data) {
+   listOfContact.innerHTML = "";
+   data.forEach((row, index) => {
+       if(index != 0 && row.name != '' && row.phone != '' && row.email != '') // По индексу 0 приходят названия столбцов. Остальное проверка важных ячеек, чтоб не выводилась пустота
+       {
+           listOfContact.innerHTML += "<li class=\"list-group-item mb1\">\n" +
+           "            <div class=\"d-flex flex-row align-items-center\">\n" +
+           "                <a class=\"nav-link d-flex flex-fill\" data-bs-toggle=\"collapse\" href=\"#id" + index + "\">\n" +
+           row.name +
+           "                </a>\n" +
+           "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"editContactFunction(this)\" data-phone='" + row.phone + "'>🖊</span>Edit</button>\n" +
+           "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"deleteContactFunction(this)\" data-phone='" + row.phone + "'>🗑</span>Delete</button>\n" +
+           "            </div>\n" +
+           "            <div class=\"collapse\" id=\"id" + index + "\">\n" +
+           "                <div class=\"card card-body\">\n" +
+           "                    <p>Company: " + row.company +
+           "                    </p><p>Group: " + row.group +
+           "                    </p><p>Birthday: " + row.birthday +
+           "                    </p><p>Phone: " + row.phone +
+           "                    </p><p>Email: " + row.email +
+           "                    </p><p>Address: " + row.address +
+           "                    </p><p>Last Call: " + row.lastCall +
+           "                    </p><p>Addition Info: " + row.addition +
+           "                    </p><p>Description: " + row.description +
+           "                </div>\n" +
+           "            </div>\n" +
+           "        </li>"
+       }
+   })
 }
 
 function editContactFunction(object) {
@@ -112,7 +116,17 @@ function editContactFunction(object) {
 
 function deleteContactFunction(object) {
     //delete item by phone
-    window.alert(object.getAttribute("data-phone"));
+    const formData = new FormData();
+    formData.append('operation', 'deleteContact');
+    formData.append('phone', object.getAttribute("data-phone"));
+    fetch(scriptUrl,{
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.json())          
+        .then(data => {
+            showAllContacts();
+        })
 }
 
 function searchContact(field) {
@@ -130,5 +144,3 @@ function importContacts() {
 function reminder() {
 
 }
-
-
