3d5b5de3901fb368a84324909d9ce9a57a2042ca
KorotkyiAnton <anton.korotkyi@nure.ua>
2022-06-29 18:54:57 +0300

    Refactoring -fix small syntax mistakes - (index.html, main.js)

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
  * decoration tools
  */
 const scriptUrl = 'https://script.google.com/macros/s/AKfycbza2vNeT_CPPD4yjzvcFVgSlseaDflsJJOBOXww-VBTthZ1iTcG6fkVOElSSYl_DBboDg/exec'; // Ссылка на развернутое веб-приложение gas
-let dataOnSite; // Данные которые сейчас на экране
+var dataOnSite; // Данные которые сейчас на экране
 window.onload = () => {
     const importContactForm = document.getElementById("importContactForm");
     contactForm.classList.add("collapse");
@@ -12,25 +12,25 @@ window.onload = () => {
 }
 
 // Функция для вывода формы добавления контакта
-function AdderSettings() {
+function AdderSettings(){
     // вывод формы добавления контакта
     listOfContact.classList.add("collapse");
     importContactForm.classList.add("collapse");
     contactForm.classList.remove("collapse");
     // Установка полей пустыми
-    document.getElementById("name").setAttribute('value', '');
-    document.getElementById("company").setAttribute('value', '');
-    document.getElementById("group").setAttribute('value', '');
-    document.getElementById("birthday").setAttribute('value', '');
-    document.getElementById("phone").setAttribute('value', '');
-    document.getElementById("email").setAttribute('value', '');
-    document.getElementById("address").setAttribute('value', '');
-    document.getElementById("additionalInfo").setAttribute('value', '');
-    document.getElementById("description").setAttribute('value', '');
+    document.getElementById("name").setAttribute('value','');
+    document.getElementById("company").setAttribute('value','');
+    document.getElementById("group").setAttribute('value','');
+    document.getElementById("birthday").setAttribute('value','');
+    document.getElementById("phone").setAttribute('value','');
+    document.getElementById("email").setAttribute('value','');
+    document.getElementById("address").setAttribute('value','');
+    document.getElementById("additionalInfo").setAttribute('value','');
+    document.getElementById("description").setAttribute('value','');
 }
 
 // Функция для обработки нажатия на кнопку добавления/изменения контакта
-function SubmitBtn() {
+function SubmitBtn(){
     // Установка полей
     const nameInput = document.getElementById("name");
     const companyInput = document.getElementById("company");
@@ -39,14 +39,14 @@ function SubmitBtn() {
     const emailInput = document.getElementById("email");
     const addressInput = document.getElementById("address");
     const birthdayInput = document.getElementById("birthday");
-    const additionalInput = document.getElementById("additionalInfo");
+    const additionInput = document.getElementById("additionalInfo");
     const descriptionInput = document.getElementById("description");
     // Вызов функции добавления/изменения
-    addPostData(nameInput, companyInput, groupInput, phoneInput, emailInput, addressInput, birthdayInput, additionalInput, descriptionInput);
+    addPostData(nameInput, companyInput, groupInput, phoneInput, emailInput, addressInput, birthdayInput, additionInput, descriptionInput);
 }
 
-// Функция добавления/изменения контакта 
-function addPostData(nameInput, companyInput, groupInput, phoneInput, emailInput, addressInput, birthdayInput) {
+// Функция добавления/изменения контакта
+function addPostData(nameInput, companyInput, groupInput, phoneInput, emailInput, addressInput, birthdayInput, additionInput, descriptionInput) {
     const formData = new FormData();
     const mainLogo = document.getElementById("main");
     // Указание типа операции
@@ -59,20 +59,19 @@ function addPostData(nameInput, companyInput, groupInput, phoneInput, emailInput
     formData.append('email', emailInput.value);
     formData.append('address', addressInput.value);
     formData.append('birthday', birthdayInput.value);
-    formData.append('additionalInfo', additionalInput.value);
+    formData.append('addition', additionInput.value);
     formData.append('description', descriptionInput.value);
     // Передача данных
-    fetch(scriptUrl, {
+    fetch(scriptUrl,{
         method: 'POST',
         body: formData
     })
         .then(res => res.json())
-        .then(() => {
+        .then(data => {
             // Перезагружаем страницу
             mainLogo.click();
         })
 }
-
 // Функция вывода всех контактов
 function showAllContacts() {
     // Отправляется запрос
@@ -91,7 +90,7 @@ function addGotData(data) {
     listOfContact.innerHTML = "";
     //За каждую строку в таблице получаем по ряду
     data.forEach((row, index) => {
-        if (row.name !== '' && row.phone !== '' && row.email !== '') // Проверка важных ячеек, чтоб не выводилась пустота
+        if(row.name !== '' && row.phone !== '' && row.email !== '') // Проверка важных ячеек, чтоб не выводилась пустота
         {
             listOfContact.innerHTML += "<li class=\"list-group-item mb-1\">\n" +
                 "            <div class=\"d-flex flex-row align-items-center\">\n" +
@@ -101,7 +100,7 @@ function addGotData(data) {
                 //Кнопка звонка
                 "               <button type=\"button\" class=\"btn btn-icon\" onclick=\"updateLastCall(this)\" data-phone='" + row.phone + "'><span class='phone me-1'></span>Call</button>\n" +
                 //Кнопка изменения контакта
-                "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"editContactFunction(this)\" data-name='" + row.name + "' data-company='" + row.company + "' data-group='" + row.group + "' data-birthday='" + row.birthday + "' data-phone='" + row.phone + "' data-email='" + row.email + "' data-address='" + row.address + "' data-lastCall='" + row.lastCall + "' data-addition='" + row.addition + "' data-Description='" + row.description + "'><span class='pen me-1'></span>Edit</button>\n" +
+                "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"editContactFunction(this)\" data-name='" + row.name +"'data-company='" + row.company +"'data-group='" + row.group +"'data-birthday='" + row.birthday +"'data-phone='" + row.phone +"'data-email='" + row.email +"'data-address='" + row.address +"'data-lastCall='" + row.lastCall +"'data-addition='" + row.addition +"'data-description='" + row.description +  "'><span class='pen me-1'></span>Edit</button>\n" +
                 //Кнопка удаления контакта
                 "                <button type=\"button\" class=\"btn btn-icon\" onclick=\"deleteContactFunction(this)\" data-phone='" + row.phone + "'><span class='trash me-1'></span>Delete</button>\n" +
                 "            </div>\n" +
@@ -122,7 +121,6 @@ function addGotData(data) {
         }
     })
 }
-
 // Функция изменения контакта
 function editContactFunction(object) {
     // Выводим форму для заполнения
@@ -130,17 +128,16 @@ function editContactFunction(object) {
     importContactForm.classList.add("collapse");
     contactForm.classList.remove("collapse");
     // Заполняем поля уже имеющимися данными
-    document.getElementById("name").setAttribute('value', object.getAttribute("data-name"));
-    document.getElementById("company").setAttribute('value', object.getAttribute("data-company"));
-    document.getElementById("group").setAttribute('value', object.getAttribute("data-group"));
-    document.getElementById("birthday").setAttribute('value', object.getAttribute("data-birthday"));
-    document.getElementById("phone").setAttribute('value', object.getAttribute("data-phone"));
-    document.getElementById("email").setAttribute('value', object.getAttribute("data-email"));
-    document.getElementById("address").setAttribute('value', object.getAttribute("data-address"));
-    document.getElementById("additionalInfo").setAttribute('value', object.getAttribute("data-addition"));
-    document.getElementById("description").setAttribute('value', object.getAttribute("data-description"));
+    document.getElementById("name").setAttribute('value',object.getAttribute("data-name"));
+    document.getElementById("company").setAttribute('value',object.getAttribute("data-company"));
+    document.getElementById("group").setAttribute('value',object.getAttribute("data-group"));
+    document.getElementById("birthday").setAttribute('value',object.getAttribute("data-birthday"));
+    document.getElementById("phone").setAttribute('value',object.getAttribute("data-phone"));
+    document.getElementById("email").setAttribute('value',object.getAttribute("data-email"));
+    document.getElementById("address").setAttribute('value',object.getAttribute("data-address"));
+    document.getElementById("additionalInfo").setAttribute('value',object.getAttribute("data-addition"));
+    document.getElementById("description").setAttribute('value',object.getAttribute("data-description"));
 }
-
 // Функция удаления контакта
 function deleteContactFunction(object) {
     const formData = new FormData();
@@ -149,17 +146,16 @@ function deleteContactFunction(object) {
     // Передача номера в качестве параметра
     formData.append('phone', object.getAttribute("data-phone"));
     // Запрос
-    fetch(scriptUrl, {
+    fetch(scriptUrl,{
         method: 'POST',
         body: formData
     })
         .then(res => res.json())
-        .then(() => {
+        .then(data => {
             // Выводим все контакты
             showAllContacts();
         })
 }
-
 // Функция поиска данных
 function searchContact() {
     const searchSelect = document.getElementById("searchSelect");
@@ -172,7 +168,7 @@ function searchContact() {
     // Указываем значение по которому идёт поиск
     formData.append('value', searchInput.value);
     // Посылаем запрос
-    fetch(scriptUrl, {
+    fetch(scriptUrl,{
         method: 'POST',
         body: formData
     })
@@ -187,9 +183,8 @@ function searchContact() {
     importContactForm.classList.add("collapse");
     contactForm.classList.add("collapse");
 }
-
 // Функция экспорта контактов
-function exportContacts() {
+function exportContacts(path) {
     //Создаём файл со значением наших данных
     var a = document.createElement("a");
     var file = new Blob([JSON.stringify(dataOnSite)], {type: "application/json"});
@@ -198,29 +193,30 @@ function exportContacts() {
     //Инициируем скачивание
     a.click();
 }
-
 //Функция поиска файла для импорта
 function getImportFile() {
     //Инициируем нажатие на fileInput
-    fileInput = document.getElementById("file");
+    fileInput=document.getElementById("file");
     fileInput.click();
 }
-
 //Функция импорта
 function importContacts() {
     //Ищем файл
-    fileInput = document.getElementById("file");
+    fileInput=document.getElementById("file");
     getImportFile()
     //Когда нашли:
-    fileInput.onchange = function () {
+    fileInput.onchange = function(e) {
         //Валидируем получение файла
         if (!fileInput) {
             alert("Um, couldn't find the fileinput element.");
-        } else if (!fileInput.files) {
+        }
+        else if (!fileInput.files) {
             alert("This browser doesn't seem to support the `files` property of file inputs.");
-        } else if (!fileInput.files[0]) {
+        }
+        else if (!fileInput.files[0]) {
             alert("Please select a file before clicking 'Load'");
-        } else {
+        }
+        else {
             //Файл корректен, читаем данные, отправляем данные в таблицу
             file = fileInput.files[0];
             fr = new FileReader();
@@ -232,17 +228,18 @@ function importContacts() {
         function receivedText(e) {
             //Парсим данные
             let lines = e.target.result;
+            var newArr = JSON.parse(lines);
             //Добавляем данные в форму
             const formData = new FormData();
             formData.append('operation', 'importContacts'); // тип операции
             formData.append('data', lines); // номер телефона
             //Отправляем запрос с формой в параметре
-            fetch(scriptUrl, {
+            fetch(scriptUrl,{
                 method: 'POST',
                 body: formData
             })
                 .then(res => res.json())
-                .then(() => {
+                .then(data => {
                     showAllContacts(); // показать все данные в таблице
                 })
         }
@@ -255,15 +252,14 @@ function reminder() {
     // Указываем операцию
     formData.append('operation', 'reminder');
     // Делаем запрос напоминания
-    fetch(scriptUrl, {
+    fetch(scriptUrl,{
         method: 'POST',
         body: formData
     })
         .then(res => res.json())
-        .then(() => {
+        .then(data => {
         })
 }
-
 // Устанавливаем интервал напоминания
 setInterval(reminder, 86400000);
 
@@ -275,12 +271,12 @@ function updateLastCall(object) {
     // Указываем номер телефона
     formData.append('phone', object.getAttribute("data-phone"));
     // Отправляем запрос
-    fetch(scriptUrl, {
+    fetch(scriptUrl,{
         method: 'POST',
         body: formData
     })
         .then(res => res.json())
-        .then(() => {
+        .then(data => {
             showAllContacts();// показать все данные в таблице
         })
 }
